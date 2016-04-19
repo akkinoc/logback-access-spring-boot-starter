14d9390f5b59b380be90ce5387a6096ddfa796b0
Akihiro Kondo <akihyro@gmail.com>
2016-04-19 20:11:02 +0900

    AccessEventAssert: Add verification methods

--- src/test/java/net/rakugakibox/springbootext/logback/access/test/AccessEventAssert.java
+++ src/test/java/net/rakugakibox/springbootext/logback/access/test/AccessEventAssert.java
@@ -1,7 +1,11 @@
 package net.rakugakibox.springbootext.logback.access.test;
 
 import ch.qos.logback.access.spi.IAccessEvent;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import org.assertj.core.api.AbstractAssert;
+import org.assertj.core.api.Assertions;
 
 /**
  * The assertion of the {@link IAccessEvent}.
@@ -25,6 +29,31 @@ public class AccessEventAssert<S extends AccessEventAssert<S, A>, A extends IAcc
      */
     protected AccessEventAssert(A actual, Class<?> selfClass) {
         super(actual, selfClass);
+    }
+
+    /**
+     * Verifies that the timestamp in the actual value is in given range.
+     *
+     * @param start the start value of range (inclusive).
+     * @param end the end value of range (inclusive).
+     * @return this instance.
+     */
+    public S hasTimestampOfBetween(LocalDateTime start, LocalDateTime end) {
+        LocalDateTime timestamp = LocalDateTime.ofInstant(
+                Instant.ofEpochMilli(actual.getTimeStamp()), ZoneId.systemDefault());
+        Assertions.assertThat(timestamp).isAfterOrEqualTo(start).isBeforeOrEqualTo(end);
+        return myself;
+    }
+
+    /**
+     * Verifies that the content length in the actual value is equal to the given one.
+     *
+     * @param expected the expected content length.
+     * @return this instance.
+     */
+    public S hasContentLength(long expected) {
+        Assertions.assertThat(actual.getContentLength()).isEqualTo(expected);
+        return myself;
     }
 
     /**
