e7589b0c64b09df9a59f534231e51036ad60d257
Akihiro Kondo <akihyro@gmail.com>
2016-04-19 18:05:05 +0900

    Add AccessEventAssert class for test

---
+++ src/test/java/net/rakugakibox/springbootext/logback/access/test/AccessEventAssert.java
@@ -0,0 +1,41 @@
+package net.rakugakibox.springbootext.logback.access.test;
+
+import ch.qos.logback.access.spi.IAccessEvent;
+import org.assertj.core.api.AbstractAssert;
+
+/**
+ * The {@link IAccessEvent} assertion.
+ */
+public class AccessEventAssert<S extends AccessEventAssert<S, A>, A extends IAccessEvent> extends AbstractAssert<S, A> {
+
+    /**
+     * Constructs an instance.
+     *
+     * @param actual the actual value.
+     */
+    protected AccessEventAssert(A actual) {
+        super(actual, AccessEventAssert.class);
+    }
+
+    /**
+     * Constructs an instance.
+     *
+     * @param actual the actual value.
+     * @param selfClass the self class.
+     */
+    protected AccessEventAssert(A actual, Class<?> selfClass) {
+        super(actual, selfClass);
+    }
+
+    /**
+     * Starts the assertion.
+     *
+     * @param <A> the actual value type.
+     * @param actual the actual value.
+     * @return an assertion instance.
+     */
+    public static <A extends IAccessEvent> AccessEventAssert<?, A> assertThat(A actual) {
+        return new AccessEventAssert<>(actual);
+    }
+
+}
