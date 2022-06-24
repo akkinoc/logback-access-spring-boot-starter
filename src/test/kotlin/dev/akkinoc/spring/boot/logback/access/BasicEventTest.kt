9eb928cac32fa4eb16f6e2050d9489cf7dd13abc
Akihiro Kondo <akkinoc@gmail.com>
2022-06-24 14:11:59 +0900

    Fixed import package for @LocalServerPort

--- src/test/kotlin/dev/akkinoc/spring/boot/logback/access/BasicEventTest.kt
+++ src/test/kotlin/dev/akkinoc/spring/boot/logback/access/BasicEventTest.kt
@@ -34,7 +34,7 @@ import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.boot.test.web.client.exchange
-import org.springframework.boot.web.server.LocalServerPort
+import org.springframework.boot.test.web.server.LocalServerPort
 import org.springframework.http.RequestEntity
 import org.springframework.test.context.TestPropertySource
 import java.lang.System.currentTimeMillis
