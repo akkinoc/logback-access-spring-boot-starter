fdf3155528e454855d930f788620d45061817ffc
Akihiro Kondo <akkinoc@gmail.com>
2026-03-20 22:16:12 +0900

    Use `@AutoConfiguration` annotation

--- src/main/kotlin/dev/akkinoc/spring/boot/logback/access/LogbackAccessAutoConfiguration.kt
+++ src/main/kotlin/dev/akkinoc/spring/boot/logback/access/LogbackAccessAutoConfiguration.kt
@@ -6,13 +6,14 @@ import dev.akkinoc.spring.boot.logback.access.tee.LogbackAccessTeeServletFilterC
 import dev.akkinoc.spring.boot.logback.access.tomcat.LogbackAccessTomcatConfiguration
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory.getLogger
-import org.springframework.boot.autoconfigure.AutoConfigureBefore
+import org.springframework.boot.autoconfigure.AutoConfiguration
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.boot.web.server.autoconfigure.reactive.ReactiveWebServerConfiguration
+import org.springframework.boot.web.server.autoconfigure.servlet.ServletWebServerConfiguration
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
 import org.springframework.core.env.Environment
 import org.springframework.core.io.ResourceLoader
@@ -20,13 +21,7 @@ import org.springframework.core.io.ResourceLoader
 /**
  * The auto-configuration for Logback-access.
  */
-@Configuration(proxyBeanMethods = false)
-@AutoConfigureBefore(
-    name = [
-        "org.springframework.boot.web.server.autoconfigure.servlet.ServletWebServerConfiguration",
-        "org.springframework.boot.web.server.autoconfigure.reactive.ReactiveWebServerConfiguration",
-    ],
-)
+@AutoConfiguration(before = [ServletWebServerConfiguration::class, ReactiveWebServerConfiguration::class])
 @ConditionalOnProperty(prefix = "logback.access", name = ["enabled"], havingValue = "true", matchIfMissing = true)
 @ConditionalOnWebApplication
 @EnableConfigurationProperties(LogbackAccessProperties::class)
