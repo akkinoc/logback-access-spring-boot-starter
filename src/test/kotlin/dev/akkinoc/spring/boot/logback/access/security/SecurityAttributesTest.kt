99abc743f20eaaeca5850b2ab71a4e5d60eaa195
Akihiro Kondo <akkinoc@gmail.com>
2026-03-21 02:45:30 +0900

    Move SecurityAttributesTest to the security package

--- src/test/kotlin/dev/akkinoc/spring/boot/logback/access/security/SecurityAttributesTest.kt
+++ src/test/kotlin/dev/akkinoc/spring/boot/logback/access/security/SecurityAttributesTest.kt
@@ -1,4 +1,4 @@
-package dev.akkinoc.spring.boot.logback.access
+package dev.akkinoc.spring.boot.logback.access.security
 
 import dev.akkinoc.spring.boot.logback.access.test.assertion.Assertions.assertLogbackAccessEventsEventually
 import dev.akkinoc.spring.boot.logback.access.test.extension.EventsCapture
