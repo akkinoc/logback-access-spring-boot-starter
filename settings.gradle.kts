eb5bda533bba612c0cccb6e3414d524d24c6b42f
Akihiro Kondo <akkinoc@gmail.com>
2026-05-16 01:09:09 +0900

    Add gradle plugin: org.gradle.toolchains.foojay-resolver-convention

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,13 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
+
 rootProject.name = "logback-access-spring-boot-starter"
 
 include("logback-access-spring-boot-starter")
@@ -6,9 +16,3 @@ include("examples:webmvc-tomcat-kotlin")
 include("examples:webmvc-jetty-java")
 include("examples:webflux-tomcat-java")
 include("examples:webflux-jetty-java")
-
-dependencyResolutionManagement {
-    repositories {
-        mavenCentral()
-    }
-}
